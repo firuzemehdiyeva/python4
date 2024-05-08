17de99194dae2f4a26761ab7c3a20d21a43bc78a
firuze mehdiyeva <firuzemehdiyeva06@gmail.com>
2024-05-08 17:56:44 +0400

    first commit

---
+++ lesson4.py
@@ -0,0 +1,26 @@
+
+# class Das:
+#     def __init__(self):
+#         print("Sahmat taxtasi yaradildi")
+    
+#     def hereket(self):
+#         print("Das hereket edir.")
+        
+# class Top(Das):
+#     def __init__(self, ad, reng, getdiyi_yer):
+#         super().__init__()
+#         print("Top yaradıldı")
+#         self.ad = ad
+#         self.reng = reng
+#         self.getdiyi_yer = getdiyi_yer
+
+#     def hereket_et(self, indiki_getdiyi_yer):
+#         print(f"{self.reng} rengli {self.ad} {self.getdiyi_yer} konumundan {indiki_getdiyi_yer} konumuna hereket edir.")
+#         self.getdiyi_yer = indiki_getdiyi_yer
+    
+#     def hereket(self):
+#         print(f"{self.reng} rengli {self.ad} hereket edir.")
+
+# ag_top = Top("Top", "ag", (0, 0))
+# ag_top.hereket_et((1, 2))
+# ag_top.hereket()
